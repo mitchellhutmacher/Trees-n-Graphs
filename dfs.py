e65d4a313b65d60323df360f786385f69290e0eb
Mitchell Hutmacher <mitchellhutmacher@gmail.com>
2022-07-18 20:40:41 -0500

    Added basic pre_order implementation

--- dfs.py
+++ dfs.py
@@ -1,13 +1,25 @@
 from .Tree import Tree
 
 
-def pre_order(root: Tree):
+def pre_order(root: Tree, order=[]):
     """
     Searches through the tree and returns the pre-order
+    :param order: The pre-order of the tree
     :param root: Tree to be searched
     :return: Pre-order of the tree
     """
-    return None
+    if root is None:
+        return order
+
+    order.append(root.val)
+    if root.left is None and root.right is None:
+        return order
+    else:
+        if root.left is not None:
+            order = pre_order(root.left, order)
+        if root.right is not None:
+            order = pre_order(root.right, order)
+    return order
 
 
 def in_order(root: Tree):
