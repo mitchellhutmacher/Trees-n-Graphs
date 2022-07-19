49f25f852b3b4aec3c01f01c66830159b54514ef
Mitchell Hutmacher <mitchellhutmacher@gmail.com>
2022-07-18 20:32:44 -0500

    Stubbed new functions

---
+++ dfs.py
@@ -0,0 +1,28 @@
+from .Tree import Tree
+
+
+def pre_order(root: Tree):
+    """
+    Searches through the tree and returns the pre-order
+    :param root: Tree to be searched
+    :return: Pre-order of the tree
+    """
+    return None
+
+
+def in_order(root: Tree):
+    """
+    Searches through the tree and returns the in-order
+    :param root: Tree to be searched
+    :return: In-order of the tree
+    """
+    return None
+
+
+def post_order(root: Tree):
+    """
+    Searches through the tree and returns the post-order
+    :param root: Tree to be searched
+    :return: Post-order of the tree
+    """
+    return None
